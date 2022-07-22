16a8be9cd91793c195afb16b2c617a4bb16b39ed
Aly Mohamed <asecure05@gmail.com>
2022-07-22 14:53:27 +0200

    Initial commit

---
+++ app/src/main/java/com/silverbullet/asteroidsradar/AsteroidsApp.kt
@@ -0,0 +1,14 @@
+package com.silverbullet.asteroidsradar
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
+
+@HiltAndroidApp
+class AsteroidsApp: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        Timber.plant(Timber.DebugTree())
+    }
+}
---
+++ app/src/main/java/com/silverbullet/asteroidsradar/ui/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.silverbullet.asteroidsradar.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.silverbullet.asteroidsradar.R
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
