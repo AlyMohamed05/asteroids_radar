c4f64452a4f2909d580d7d0f7cc968647f4b24c7
Aly Mohamed <asecure05@gmail.com>
2022-07-22 15:41:03 +0200

    Adding very basic navigation, created home fragment and finished initialization.

---
+++ app/src/main/java/com/silverbullet/asteroidsradar/ui/fragments/home/HomeFragment.kt
@@ -0,0 +1,26 @@
+package com.silverbullet.asteroidsradar.ui.fragments.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.silverbullet.asteroidsradar.databinding.HomeFragmentBinding
+
+class HomeFragment : Fragment() {
+
+    private lateinit var binding: HomeFragmentBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = HomeFragmentBinding.inflate(
+            inflater,
+            container,
+            false
+        )
+        return binding.root
+    }
+}
